0c08ca3c9dcd195a7f7516b2b9f0be590e9e0e27
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-02-06 23:11:09 +0700

    Migrate to mapping

--- modules/config/build.go
+++ modules/config/build.go
@@ -1,118 +1,97 @@
 package config
 
 import (
+	"context"
 	"errors"
 	"net/netip"
 
 	"github.com/FoolVPN-ID/tool/common"
 	"github.com/FoolVPN-ID/tool/modules/provider"
+	box "github.com/sagernet/sing-box"
 	C "github.com/sagernet/sing-box/constant"
+	"github.com/sagernet/sing-box/include"
 	"github.com/sagernet/sing-box/option"
 	CM "github.com/sagernet/sing/common"
+	"github.com/sagernet/sing/common/json"
 	"github.com/sagernet/sing/common/json/badoption"
 )
 
 func BuildSingboxConfig(rawConfig string) (option.Options, error) {
-	outbound, err := provider.Parse(rawConfig)
+	ctx := context.Background()
+	ctx = box.Context(ctx, include.InboundRegistry(), include.OutboundRegistry(), include.EndpointRegistry(), include.DNSTransportRegistry())
+
+	outbounds, err := provider.Parse(rawConfig)
 	if err != nil {
 		return option.Options{}, err
 	}
-	if len(outbound) == 0 {
+	if len(outbounds) == 0 {
 		return option.Options{}, errors.New("parsing failed")
 	}
 
-	config := option.Options{
-		Log: &option.LogOptions{
-			Disabled: true,
+	var outboundsAny []any
+	outboundsByte, _ := json.Marshal(outbounds)
+	json.Unmarshal(outboundsByte, &outboundsAny)
+
+	config := map[string]any{
+		"log": map[string]any{
+			"disabled": true,
 		},
-		DNS: &option.DNSOptions{
-			RawDNSOptions: option.RawDNSOptions{
-				Servers: []option.NewDNSServerOptions{
-					{
-						Tag:  "default-dns",
-						Type: "udp",
-						Options: option.RemoteDNSServerOptions{
-							ServerOptions: option.ServerOptions{
-								Server:     "1.1.1.1",
-								ServerPort: 53,
-							},
-							LocalDNSServerOptions: option.LocalDNSServerOptions{
-								DialerOptions: option.DialerOptions{
-									Detour: "direct",
-								},
-							},
-						},
-					},
-				},
-				Final: "default-dns",
-			},
-		},
-		Inbounds: []option.Inbound{
-			{
-				Type: C.TypeMixed,
-				Options: option.HTTPMixedInboundOptions{
-					ListenOptions: option.ListenOptions{
-						Listen:     CM.Ptr(badoption.Addr(netip.IPv4Unspecified())),
-						ListenPort: uint16(common.GetFreePort()),
-					},
-				},
-			},
-		},
-		Outbounds: []option.Outbound{
-			outbound[0],
-			{
-				Tag:  "direct",
-				Type: C.TypeDirect,
-			},
-		},
-		Route: &option.RouteOptions{
-			Rules: []option.Rule{
+		"dns": map[string]any{
+			"servers": []map[string]any{
 				{
-					Type: C.RuleTypeDefault,
-					LogicalOptions: option.LogicalRule{
-						RawLogicalRule: option.RawLogicalRule{
-							Mode:   "or",
-							Invert: false,
-							Rules: []option.Rule{
-								{
-									DefaultOptions: option.DefaultRule{
-										RawDefaultRule: option.RawDefaultRule{
-											Protocol: badoption.Listable[string]{"dns"},
-										},
-									},
-								},
-								{
-									DefaultOptions: option.DefaultRule{
-										RawDefaultRule: option.RawDefaultRule{
-											Port: badoption.Listable[uint16]{53},
-										},
-									},
-								},
-							},
+					"tag":         "default-dns",
+					"type":        "udp",
+					"server":      "1.1.1.1",
+					"server_port": 53,
+					"detour":      "direct",
+				},
+			},
+			"final": "default-dns",
+		},
+		"inbounds": []map[string]any{
+			{
+				"tag":         "mixed-in",
+				"type":        C.TypeMixed,
+				"listen":      CM.Ptr(badoption.Addr(netip.IPv4Unspecified())),
+				"listen_port": uint16(common.GetFreePort()),
+			},
+		},
+		"outbounds": []map[string]any{
+			{
+				"tag":  "direct",
+				"type": C.TypeDirect,
+			},
+		},
+		"route": map[string]any{
+			"rules": []map[string]any{
+				{
+					"type": C.RuleTypeLogical,
+					"mode": "or",
+					"rules": []map[string]any{
+						{
+							"protocol": "dns",
 						},
-						RuleAction: option.RuleAction{
-							Action: "hijack-dns",
+						{
+							"port": 53,
 						},
 					},
+					"action": "hijack-dns",
 				},
 				{
-					Type: C.RuleTypeDefault,
-					DefaultOptions: option.DefaultRule{
-						RawDefaultRule: option.RawDefaultRule{
-							Network: badoption.Listable[string]{"udp"},
-						},
-						RuleAction: option.RuleAction{
-							Action: "route",
-							RouteOptions: option.RouteActionOptions{
-								Outbound: "direct",
-							},
-						},
-					},
+					"type":     C.RuleTypeDefault,
+					"network":  "udp",
+					"action":   "route",
+					"outbound": "direct",
 				},
 			},
-			Final: outbound[0].Tag,
+			"final": outbounds[0].Tag,
 		},
 	}
 
-	return config, nil
+	for _, outbound := range outboundsAny {
+		config["outbounds"] = append([]map[string]any{outbound.(map[string]any)}, config["outbounds"].([]map[string]any)...)
+	}
+
+	configByte, _ := json.Marshal(config)
+	return json.UnmarshalExtendedContext[option.Options](ctx, configByte)
 }
